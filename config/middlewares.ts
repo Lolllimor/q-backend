1f2c1c79e97635db054d5301cae6f4364da1a192
Lolllimor <ololade_morn@hotmail.com>
2025-11-23 00:17:24 +0100

    feat: adding data

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -1,8 +1,38 @@
 export default [
   'strapi::logger',
   'strapi::errors',
-  'strapi::security',
-  'strapi::cors',
+
+  // SECURITY MUST COME FIRST
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'connect-src': ["'self'", 'https:', '*'],
+          'img-src': ["'self'", 'data:', 'blob:', '*'],
+          'media-src': ["'self'", 'data:', 'blob:', '*'],
+          upgradeInsecureRequests: null,
+        },
+      },
+      crossOriginEmbedderPolicy: false,
+      crossOriginResourcePolicy: false,
+      crossOriginOpenerPolicy: false,
+    },
+  },
+
+  // THEN CORS
+  {
+    name: 'strapi::cors',
+    config: {
+      enabled: true,
+      origin: ['*'], // allow localhost + production
+      headers: '*', // allow Authorization header => CRITICAL
+      methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+      credentials: true,
+    },
+  },
+
   'strapi::poweredBy',
   'strapi::query',
   'strapi::body',

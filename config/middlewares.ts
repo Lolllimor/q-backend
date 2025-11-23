364968b8f09497b6f741fb9a6f8f22c17e6e7bc4
Lolllimor <ololade_morn@hotmail.com>
2025-11-23 01:11:27 +0100

    feat: adding data

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -1,29 +1,8 @@
 export default [
   'strapi::logger',
   'strapi::errors',
-
-  // SECURITY — CSP disabled for now
-  {
-    name: 'strapi::security',
-    config: {
-      contentSecurityPolicy: false,
-      crossOriginEmbedderPolicy: false,
-      crossOriginResourcePolicy: false,
-      crossOriginOpenerPolicy: false,
-    },
-  },
-
-  // CORS
-  {
-    name: 'strapi::cors',
-    config: {
-      origin: ['http://localhost:3000', 'https://your-production-frontend.com'],
-      methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
-      headers: '*',
-      credentials: true,
-    },
-  },
-
+  'strapi::security',
+  'strapi::cors',
   'strapi::poweredBy',
   'strapi::query',
   'strapi::body',
